6c56da2540fd6a36b60f2d71000b69951a1c0be0
trainercsl <trainercsl28@gmail.com>
2026-05-12 18:33:16 +0530

    testing file

---
+++ demo.py
@@ -0,0 +1,11 @@
+status="start"
+while status=="start":
+    user=input("user input:")
+    if "hello" in user:
+        print("AI output: hello sir")
+    elif "who are you?" == user:
+        print("AI output: i am AI")
+    elif "bye" == user:
+        status="stop"
+    else:
+        print("invalid")
